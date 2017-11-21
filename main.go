27ce9f53ea37ecd4a9508446109d1a7f3873f10d
DarthHater <jeffryxtron@gmail.com>
2017-11-21 17:11:37 +0000

    Reorg funcs, add license header

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+/* Copyright 2017 Jeffry Hesse
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License. */
+
 package main
 
 import (
@@ -7,6 +21,12 @@ import (
 	"github.com/toorop/gin-logrus"
 )
 
+func main() {
+	r := setupRouter()
+	r.Use(gin.Logger())
+	r.Run(":8000")
+}
+
 func setupRouter() *gin.Engine {
 	log := log.New()
 	r := gin.New()
@@ -15,12 +35,6 @@ func setupRouter() *gin.Engine {
 	r.GET("/thread", getThread)
 
 	return r
-}
-
-func main() {
-	r := setupRouter()
-	r.Use(gin.Logger())
-	r.Run(":8000")
 }
 
 // Handlers
