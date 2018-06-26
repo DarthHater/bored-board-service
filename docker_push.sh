3c61991da51fe5fc9401be845675fab5580c3da8
DarthHater <jeffryxtron@gmail.com>
2018-06-25 17:15:01 -0800

    Welp

--- docker_push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
-docker build -t registry.heroku.com/vivalavinyl/vivalavinyl .
-docker push registry.heroku.com/vivalavinyl/vivalavinyl docker login -u _ -p $HEROKU_TOKEN registry.heroku.com
