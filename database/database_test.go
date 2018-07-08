cdc26722289f97015bbfaa5d072c63d550d46d5e
Andy <andy.s.bolton@gmail.com>
2018-07-08 16:37:53 -0600

    Ah HA

--- database/database_test.go
+++ database/database_test.go
@@ -72,14 +72,14 @@ func TestGetPost(t *testing.T) {
 	}
 	defer DB.Close()
 
-	row := sqlmock.NewRows([]string{"id", "threadid", "userid", "body", "postedat" }).
-		AddRow("", "", "", "Post Body", "A time", "Rite now")
+	row := sqlmock.NewRows([]string{"id", "threadid", "userid", "body", "postedat"}).
+		AddRow("", "", "", "Post Body", "A time")
 
 	mock.ExpectQuery("SELECT (.+) FROM board.thread_post WHERE (.+)").WillReturnRows(row)
 
 	result, err := d.GetPost("a thread")
 
-	expected := model.Post{Id: "", ThreadId: "", UserId: "", Body: "Post Body", PostedAt: "A time" }
+	expected := model.Post{Id: "", ThreadId: "", UserId: "", Body: "Post Body", PostedAt: "A time"}
 
 	assert.Equal(t, result, expected)
 
