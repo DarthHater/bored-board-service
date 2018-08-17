527087d689bbf2f29fbfa8b6ff3430848995f708
Andy <andy.s.bolton@gmail.com>
2018-08-16 18:25:51 -0600

    Migrations

--- .sql/01-schema.sql
+++ .sql/01-schema.sql
@@ -12,39 +12,45 @@ CREATE EXTENSION pgcrypto;
 
 CREATE SCHEMA board AUTHORIZATION admin;
 
-CREATE TABLE board.user
-(
-    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    Username varchar(250) UNIQUE,
-    Emailaddress varchar(250) UNIQUE,
-    UserPassword bytea,
-    UserRole int
-);
-
-CREATE TABLE board.thread
-(
-    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    UserId UUID,
-    Title varchar(250),
-    PostedAt TIMESTAMP DEFAULT now(),
-    Deleted boolean DEFAULT false
-);
-
-CREATE INDEX thread_deleted_idx ON board.thread (Deleted);
-
-CREATE TABLE board.thread_post
-(
-    Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    ThreadId UUID REFERENCES board.thread (Id),
-    UserId UUID,
-    Body text,
-    PostedAt TIMESTAMP DEFAULT now(),
-    Deleted boolean DEFAULT false
-);
-
-CREATE INDEX thread_post_deleted_idx ON board.thread_post (Deleted);
-
 GRANT ALL PRIVILEGES
     ON ALL TABLES
     IN SCHEMA board
     TO admin;
+
+
+-- \connect db;
+
+-- CREATE EXTENSION pgcrypto;
+
+-- CREATE TABLE board.user
+-- (
+--     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     Username varchar(250) UNIQUE,
+--     Emailaddress varchar(250) UNIQUE,
+--     UserPassword bytea,
+--     UserRole int
+-- );
+
+-- CREATE TABLE board.thread
+-- (
+--     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     UserId UUID,
+--     Title varchar(250),
+--     PostedAt TIMESTAMP DEFAULT now(),
+--     Deleted boolean DEFAULT false
+-- );
+
+-- CREATE INDEX thread_deleted_idx ON board.thread (Deleted);
+
+-- CREATE TABLE board.thread_post
+-- (
+--     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     ThreadId UUID REFERENCES board.thread (Id),
+--     UserId UUID,
+--     Body text,
+--     PostedAt TIMESTAMP DEFAULT now(),
+--     Deleted boolean DEFAULT false
+-- );
+
+-- CREATE INDEX thread_post_deleted_idx ON board.thread_post (Deleted);
+
---
+++ migrations/1_1_down.sql
@@ -0,0 +1,5 @@
+DROP TABLE board.user;
+DROP INDEX thread_deleted_idx;
+DROP TABLE board.thread;
+DROP INDEX thread_post_deleted_idx;
+DROP TABLE board.thread_post;
