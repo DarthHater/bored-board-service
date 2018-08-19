1f2bfc19267b5e22634b1f1aa45061132e8ebf80
Andy <andy.s.bolton@gmail.com>
2018-08-19 17:52:27 -0600

    Fix formatting

--- model/userinfo.go
+++ model/userinfo.go
@@ -2,8 +2,8 @@ package model
 
 // UserInfo is for holding information about the user.
 type UserInfo struct {
-	TotalPosts		string	`json:"totalPosts"`
+	TotalPosts	string	`json:"totalPosts"`
 	TotalThreads	string	`json:"totalThreads"`
-	DateJoined		string	`json:"dateJoined"`
-	LastPosted		string	`json:"lastPosted"`
+	DateJoined	string	`json:"dateJoined"`
+	LastPosted	string	`json:"lastPosted"`
 }
