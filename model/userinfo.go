7117af3a6adf5a506228db04d465c9b39bbf203a
Andy <andy.s.bolton@gmail.com>
2018-08-19 18:05:43 -0600

    Another attempt

--- model/userinfo.go
+++ model/userinfo.go
@@ -3,7 +3,7 @@ package model
 // UserInfo is for holding information about the user.
 type UserInfo struct {
 	TotalPosts		string	`json:"totalPosts"`
-	TotalThreads	string	`json:"totalThreads"`
+	TotalThreads		string	`json:"totalThreads"`
 	DateJoined		string	`json:"dateJoined"`
 	LastPosted		string	`json:"lastPosted"`
 }
