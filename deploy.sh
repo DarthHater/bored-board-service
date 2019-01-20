0441de5fe08790e4798e3f417fb3e8d1059e6f2e
Jeffry Hesse <jhesse@sonatype.com>
2019-01-19 17:15:24 -0900

    Limit Travis deploy to only off master and not a PR

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl https://cli-assets.heroku.com/install.sh | sh
-  docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
-  docker build -t registry.heroku.com/vivalavinyl-service/web .
-  docker push registry.heroku.com/vivalavinyl-service/web 
-  heroku container:release web -a vivalavinyl-service
-fi
+
+curl https://cli-assets.heroku.com/install.sh | sh
+docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
+docker build -t registry.heroku.com/vivalavinyl-service/web .
+docker push registry.heroku.com/vivalavinyl-service/web 
+heroku container:release web -a vivalavinyl-service
